37120f40cda373201fbadb8f5f4abd3c72359b9b
Mel <mel@Mels-MacBook-Pro.local>
2024-05-31 12:33:05 -0600

    Feat: Google Calendar Service initial setup complete

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,11 @@ Rails.application.routes.draw do
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
+  root "home#index"  # Assuming HomeController with an index action
   get "up" => "rails/health#show", as: :rails_health_check
-
+  get '/auth/:provider/callback', to: 'sessions#omniauth'
+  get '/auth/failure', to: redirect('/')
+  delete '/logout', to: 'sessions#destroy'
   # Defines the root path route ("/")
   # root "posts#index"
 end
