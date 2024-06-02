aca570bc05181ed5a1a7ac0866cd92ca3eae7560
Mel <mel@Mels-MacBook-Pro.local>
2024-06-02 12:55:45 -0600

    current work

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,7 +1,14 @@
 class SearchesController < ApplicationController
   def index
     # @results
-    
+
+    # hard coding the preselected search params
+    # random examples; will be changed; fields prob need to be adjusted too
+    @search_params = [
+      { name: 'workout_type', value: 'cardio', label: 'Cardio', selected: false },
+      { name: 'workout_type', value: 'stretching', label: 'Stretching', selected: false },
+      { name: 'workout_type', value: 'yoga', label: 'Yoga', selected: false },
+    ]
   end
 
   def show
--- spec/features/users/show_spec.rb
+++ spec/features/users/show_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe "User Show Page" do
   before :each do
     visit user_path(id: 1)
   end
+
   it "renders the header with a logout link" do
     expect(page).to have_content("PomPlanner")
     expect(page).to have_link("Logout", href: logout_path, class: "btn btn-secondary")
