4f01f97787d003902e4453ddf063b3b3d9fc45dd
Nico Shanstrom <nicoshanstrom@gmail.com>
2024-06-05 00:13:53 -0400

    Cannot figure out why i am not getting data returned from oauth flow, but the flow is going

--- config/routes.rb
+++ config/routes.rb
@@ -4,16 +4,16 @@ Rails.application.routes.draw do
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   root "home#index"  # Assuming HomeController with an index action
-  get "up" => "rails/health#show", as: :rails_health_check
-
+  
   get '/auth/:provider/callback', to: 'sessions#omniauth'
   get '/auth/failure', to: redirect('/')
   delete '/logout', to: 'sessions#destroy', as: 'logout'
-
+  
   resources :users, only: [:show]
   get '/users/:id/search', to: 'search#index'
   resources :search, only: [:index, :show]
-
+  
+  get "up" => "rails/health#show", as: :rails_health_check
   # Defines the root path route ("/")
   # root "posts#index"
 end
