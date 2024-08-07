8ec183464a1af4d3a4203424c0b8a2bf8b104cde
Nico Shanstrom <nicoshanstrom@gmail.com>
2024-08-07 14:09:12 -0400

    changes to turbo and keeps search results on page once adding to favorites

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,30 +1,40 @@
 class VideosController < ApplicationController
   before_action :set_user
 
-  def index
-    @videos = pom_planner_service.get_favorite_videos(@user.id)
-  end
-
   def create
-    # require 'pry'; binding.pry
     response = pom_planner_service.add_favorite_video(@user.id, video_params)
     if response[:status] == 201
-    # if response.success?
-      redirect_to user_path(@user.id), notice: "Video added to favorites"
+      @favorite_videos = pom_planner_service.get_favorite_videos(@user.id) || []
+      @videos = pom_planner_service.search_videos(params[:query], params[:duration]) || [] # Reassign the search results
+      respond_to do |format|
+        format.turbo_stream
+        format.html { redirect_to user_path(@user.id), notice: "Video added to favorites" }
+      end
     else
-      redirect_to user_path(@user.id), alert: "Failed to add video to favorites"
+      respond_to do |format|
+        format.turbo_stream { render turbo_stream: turbo_stream.replace("favorite-videos", partial: "videos/favorite_videos", locals: { favorite_videos: [] }) }
+        format.html { redirect_to user_path(@user.id), alert: "Failed to add video to favorites" }
+      end
     end
   end
 
   def destroy
     response = pom_planner_service.remove_favorite_video(@user.id, params[:id])
-    # require 'pry'; binding.pry
     if response.status == 200
-      redirect_to user_path(@user.id), notice: "Video removed from favorites"
+      @favorite_videos = pom_planner_service.get_favorite_videos(@user.id) || []
+      respond_to do |format|
+        format.turbo_stream
+        format.html { redirect_to user_path(@user.id), notice: "Video removed from favorites" }
+      end
     else
-      redirect_to user_path(@user.id), alert: "Failed to remove video from favorites"
+      respond_to do |format|
+        format.turbo_stream { render turbo_stream: turbo_stream.replace("favorite-videos", partial: "videos/favorite_videos", locals: { favorite_videos: [] }) }
+        format.html { redirect_to user_path(@user.id), alert: "Failed to remove video from favorites" }
+      end
     end
   end
+
+
 
   private
 
