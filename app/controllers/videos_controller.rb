b46cd3adb9b4637cc42caae576533031ce5e3ea9
Nico Shanstrom <nicoshanstrom@gmail.com>
2024-08-07 14:55:35 -0400

    still debugging why videos are not refreshing apon adding favorites

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -4,10 +4,10 @@ class VideosController < ApplicationController
   def create
     response = pom_planner_service.add_favorite_video(@user.id, video_params)
     if response[:status] == 201
-      @favorite_videos = pom_planner_service.get_favorite_videos(@user.id) || []
-      @videos = pom_planner_service.search_videos(params[:query], params[:duration]) || [] # Reassign the search results
+      @favorite_videos = pom_planner_service.get_favorite_videos(@user.id)
+      @videos = pom_planner_service.search_videos(params[:query], params[:duration])
       respond_to do |format|
-        format.turbo_stream
+        format.turbo_stream { render turbo_stream: turbo_stream.replace("favorite-videos", partial: "videos/favorite_videos", locals: { favorite_videos: @favorite_videos }) }
         format.html { redirect_to user_path(@user.id), notice: "Video added to favorites" }
       end
     else
@@ -18,12 +18,14 @@ class VideosController < ApplicationController
     end
   end
 
+
   def destroy
     response = pom_planner_service.remove_favorite_video(@user.id, params[:id])
     if response.status == 200
-      @favorite_videos = pom_planner_service.get_favorite_videos(@user.id) || []
+      @favorite_videos = pom_planner_service.get_favorite_videos(@user.id)
+      Rails.logger.debug "Favorite videos after delete: #{@favorite_videos.inspect}"
       respond_to do |format|
-        format.turbo_stream
+        format.turbo_stream { render turbo_stream: turbo_stream.replace("favorite-videos", partial: "videos/favorite_videos", locals: { favorite_videos: @favorite_videos }) }
         format.html { redirect_to user_path(@user.id), notice: "Video removed from favorites" }
       end
     else
@@ -33,8 +35,6 @@ class VideosController < ApplicationController
       end
     end
   end
-
-
 
   private
 
