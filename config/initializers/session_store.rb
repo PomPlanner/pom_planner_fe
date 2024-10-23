794a84728a1b8f80ae32f4be6993228d5b1e5ec3
Nico Shanstrom <nicoshanstrom@gmail.com>
2024-10-23 15:48:25 -0400

    Modifies session store.rb to match backend

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,1 +1,7 @@
-Rails.application.config.session_store :cookie_store, key: '_your_app_session', domain: :all, tld_length: 2, secure: Rails.env.production? ? true : false, expire_after: 1.day
+Rails.application.config.session_store :cookie_store, 
+  key: '_your_app_session', 
+  domain: :all, 
+  tld_length: 2, 
+  secure: Rails.env.production?, 
+  same_site: :none, 
+  expire_after: 7.days
