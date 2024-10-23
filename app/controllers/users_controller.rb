ae2e3fba5783d8356a90c2fd56c6bf183e6c04c3
Nico Shanstrom <nicoshanstrom@gmail.com>
2024-10-23 16:30:39 -0400

    Removes require login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :require_login, only: [:show]
+  # before_action :require_login, only: [:show]
 
   def show
     user_id = params[:id].to_i
